effcbc7a606b1b17848ee1bfbc39205d312b04d5
Lenir <1net1@naver.com>
2022-01-27 09:43:23 +0900

    Initial commit

---
+++ src/main/java/com/lenir/translate_service/TranslateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lenir.translate_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslateServiceApplication.class, args);
+	}
+
+}
